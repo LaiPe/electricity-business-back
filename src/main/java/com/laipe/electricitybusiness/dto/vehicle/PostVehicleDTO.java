85e2a34a9bac1947d3f05effb1c4a26bb2983a6f
Léo <peyronnet.leo@gmail.com>
2025-08-03 23:52:40 +0200

    fix: validation constraints on PostVehicleDTO

--- src/main/java/com/laipe/electricitybusiness/dto/vehicle/PostVehicleDTO.java
+++ src/main/java/com/laipe/electricitybusiness/dto/vehicle/PostVehicleDTO.java
@@ -3,11 +3,13 @@ package com.laipe.electricitybusiness.dto.vehicle;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class PostVehicleDTO {
     @NotBlank
+    @Size(min = 2, max = 15)
     @JsonProperty("registration_number")
     private String registrationNumber;
 
