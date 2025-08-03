2702fa1cb03d9d62a1909b422f451ee2b94b5a34
Léo <peyronnet.leo@gmail.com>
2025-08-03 17:45:58 +0200

    feat: validation on post DTOs

--- src/main/java/com/laipe/electricitybusiness/dto/user/PostUserDTO.java
+++ src/main/java/com/laipe/electricitybusiness/dto/user/PostUserDTO.java
@@ -2,30 +2,46 @@ package com.laipe.electricitybusiness.dto.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.laipe.electricitybusiness.model.UserRole;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class PostUserDTO {
+    @NotBlank
     @JsonProperty("username")
     private String username;
 
+    @NotBlank
     @JsonProperty("password")
     private String password;
 
+    @Email
+    @NotBlank
     @JsonProperty("email")
     private String email;
 
+    @NotBlank
     @JsonProperty("first_name")
     private String firstName;
 
+    @NotBlank
     @JsonProperty("last_name")
     private String lastName;
 
+    @NotNull
+    @Past
     @JsonProperty("birth_date")
     private LocalDate birthDate;
 
+    @Enumerated(EnumType.STRING)
+    @NotNull
     @JsonProperty("role")
     private UserRole role;
 
--- src/main/java/com/laipe/electricitybusiness/dto/vehicle/PostVehicleDTO.java
+++ src/main/java/com/laipe/electricitybusiness/dto/vehicle/PostVehicleDTO.java
@@ -1,16 +1,21 @@
 package com.laipe.electricitybusiness.dto.vehicle;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class PostVehicleDTO {
+    @NotBlank
     @JsonProperty("registration_number")
     private String registrationNumber;
 
+    @NotNull
     @JsonProperty("owner_id")
     private Long ownerId;
 
+    @NotBlank
     @JsonProperty("vehicle_model_id")
     private String vehicleModelId;
 }
